684a10a4244e7786f5dbe8c590b35172cc3afba6
Moaz Mohamed <moazmesmail@gmail.com>
2025-10-06 20:43:03 +0300

    FEAT: single page application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import { useContext, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import Provider from "./contexts/Context/Context.jsx";
 import Nav from "./components/Nav/Nav.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Schedule from "./pages/Schedule/Schedule.jsx";
 import Teachers from "./pages/Teachers/Teachers.jsx";
 import Teacher from "./pages/Teacher/Teacher.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
     return (
         <>
             <Provider>
-                <Nav/>
-                <Home />
-                <Schedule />
-                <Teachers />
-                <Teacher />
+                <Router>
+                    <Nav />
+                    <div className="container-xl mt-4">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/schedule" element={<Schedule />} />
+                            <Route path="/teachers" element={<Teachers />} />
+                            <Route path="/teacher/:id" element={<Teacher />} />
+                        </Routes>
+                    </div>
+                </Router>
             </Provider>
         </>
     );
